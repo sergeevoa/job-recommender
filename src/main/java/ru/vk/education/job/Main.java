db26fb8f2af12688e197f01139ffaad4d850f150
sergeevoa <sergeevolegcob@gmail.com>
2026-04-03 16:19:53 +0300

    Command logging added

--- src/main/java/ru/vk/education/job/Main.java
+++ src/main/java/ru/vk/education/job/Main.java
@@ -3,8 +3,19 @@ package ru.vk.education.job;
 import ru.vk.education.job.entity.Job;
 import ru.vk.education.job.entity.Match;
 import ru.vk.education.job.entity.User;
+import ru.vk.education.job.service.FileService;
 
-import java.util.*;
+import java.io.IOException;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.TreeSet;
+import java.util.Comparator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -42,88 +53,32 @@ public class Main {
             "^suggest\\s(\\w+)$"
     );
 
+    private static final String HISTORY_COMMAND = "history";
+
     private static final String EXIT_COMMAND = "exit";
 
     private static final Map<String, User> users = new LinkedHashMap<>();
 
     private static final Map<String, Job> jobs = new LinkedHashMap<>();
 
-    public static void main(String[] args) {
+    private static final FileService fileService = new FileService();
+
+    public static void main(String[] args) throws IOException {
+        fileService.getCommandLog().stream()
+                .filter(command -> ADD_USER_COMMAND_PATTERN.matcher(command).matches()
+                || ADD_JOB_COMMAND_PATTERN.matcher(command).matches())
+                .forEach(command -> {
+                    try {
+                        execute(command, true);
+                    } catch (IOException e) {
+                        throw new IllegalArgumentException(e);
+                    }
+                });
+
         Scanner sc = new Scanner(System.in);
 
         while(true) {
-            String command = sc.nextLine();
-
-            switch (command) {
-                case USER_LIST_COMMAND -> users.values().forEach(User::print);
-
-                case JOB_LIST_COMMAND -> jobs.values().forEach(Job::print);
-
-                case EXIT_COMMAND -> System.exit(0);
-
-                default -> {
-                    Matcher userMatcher = ADD_USER_COMMAND_PATTERN.matcher(command);
-
-                    if(userMatcher.matches()) {
-                        String name = userMatcher.group(1);
-
-                        Set<String> skills = findMatchSet(SKILLS_PATTERN, command);
-
-                        int exp = findMatchExp(command);
-
-                        users.putIfAbsent(name, new User(name, skills, exp));
-
-                    } else {
-                        Matcher jobMatcher = ADD_JOB_COMMAND_PATTERN.matcher(command);
-
-                        if(jobMatcher.matches()) {
-                            String name = jobMatcher.group(1);
-
-                            Matcher companyMatcher = COMPANY_PATTERN.matcher(command);
-
-                            String company;
-
-                            if(companyMatcher.find()) {
-                                company = companyMatcher.group(1);
-                            } else {
-                                throw new IllegalArgumentException();
-                            }
-
-                            Set<String> tags = findMatchSet(TAGS_PATTERN, command);
-
-                            int exp = findMatchExp(command);
-
-                            jobs.putIfAbsent(name, new Job(name, company, tags, exp));
-
-                        } else {
-                            Matcher suggestMatcher = SUGGEST_COMMAND_PATTERN.matcher(command);
-
-                            if(suggestMatcher.matches()) {
-                                String username = suggestMatcher.group(1);
-
-                                User user = users.get(username);
-                                if (user == null) break;
-
-                                List<Match> matches = new ArrayList<>();
-
-                                for(Job job : jobs.values()) {
-                                    matches.add(new Match(user, job));
-                                }
-
-                                matches.sort(Comparator.comparing(Match::getMatchRate).reversed());
-
-                                matches.stream()
-                                        .limit(2)
-                                        .forEach(Match::printJobInfo);
-
-                            } else {
-                                throw new IllegalArgumentException();
-                            }
-                        }
-
-                    }
-                }
-            }
+            execute(sc.nextLine(), false);
         }
     }
 
@@ -145,6 +100,103 @@ public class Main {
             return Integer.parseInt(expMatcher.group(1));
         } else {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private static void execute(String command, boolean fromLog) throws IOException {
+        switch (command) {
+            case USER_LIST_COMMAND -> {
+                users.values().forEach(User::print);
+                if(!fromLog)
+                    fileService.logCommand(command);
+            }
+
+            case JOB_LIST_COMMAND -> {
+                jobs.values().forEach(Job::print);
+                if(!fromLog)
+                    fileService.logCommand(command);
+            }
+
+            case HISTORY_COMMAND -> {
+                fileService.getCommandLog().forEach(System.out::println);
+                if(!fromLog)
+                    fileService.logCommand(command);
+            }
+
+            case EXIT_COMMAND -> System.exit(0);
+
+
+            default -> {
+                Matcher userMatcher = ADD_USER_COMMAND_PATTERN.matcher(command);
+
+                if(userMatcher.matches()) {
+                    String name = userMatcher.group(1);
+
+                    Set<String> skills = findMatchSet(SKILLS_PATTERN, command);
+
+                    int exp = findMatchExp(command);
+
+                    users.putIfAbsent(name, new User(name, skills, exp));
+
+                    if(!fromLog)
+                        fileService.logCommand(command);
+
+                } else {
+                    Matcher jobMatcher = ADD_JOB_COMMAND_PATTERN.matcher(command);
+
+                    if(jobMatcher.matches()) {
+                        String name = jobMatcher.group(1);
+
+                        Matcher companyMatcher = COMPANY_PATTERN.matcher(command);
+
+                        String company;
+
+                        if(companyMatcher.find()) {
+                            company = companyMatcher.group(1);
+                        } else {
+                            throw new IllegalArgumentException();
+                        }
+
+                        Set<String> tags = findMatchSet(TAGS_PATTERN, command);
+
+                        int exp = findMatchExp(command);
+
+                        jobs.putIfAbsent(name, new Job(name, company, tags, exp));
+
+                        if(!fromLog)
+                            fileService.logCommand(command);
+
+                    } else {
+                        Matcher suggestMatcher = SUGGEST_COMMAND_PATTERN.matcher(command);
+
+                        if(suggestMatcher.matches()) {
+                            String username = suggestMatcher.group(1);
+
+                            User user = users.get(username);
+                            if (user == null) break;
+
+                            List<Match> matches = new ArrayList<>();
+
+                            for(Job job : jobs.values()) {
+                                matches.add(new Match(user, job));
+                            }
+
+                            matches.sort(Comparator.comparing(Match::getMatchRate).reversed());
+
+                            matches.stream()
+                                    .limit(2)
+                                    .forEach(Match::printJobInfo);
+
+                            if(!fromLog)
+                                fileService.logCommand(command);
+
+                        } else {
+                            throw new IllegalArgumentException();
+                        }
+                    }
+
+                }
+            }
         }
     }
 }
